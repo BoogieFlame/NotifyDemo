6aaa700465014c36dd1fcda9c84aa5d989c0a293
BoogieFlame <kastor.vip@gmail.com>
2020-11-24 14:50:26 +0700

    Открытие ResultActivity при нажатии на уведомление

--- app/src/main/java/a/notifydemo/MainActivity.java
+++ app/src/main/java/a/notifydemo/MainActivity.java
@@ -1,9 +1,10 @@
 package a.notifydemo;
-
+import android.app.PendingIntent;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -36,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
     public void sendNotification(View view) {
 
         int notificationID = 101;
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
 
         String channelID = "a.notifydemo.news";
 
@@ -44,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 .setContentText("You've received new messages.")
                 .setSmallIcon(android.R.drawable.ic_dialog_info)
                 .setNumber(10)
+                .setContentIntent(pendingIntent)
                 .setChannelId(channelID)
                 .build();
         notificationManager.notify(notificationID, notification);
